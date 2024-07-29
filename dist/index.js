0791e1e1ba327b8dea171fbfb06759c8e279e1a5
Yehong Piao <yehongpiao@Yehongs-iMac.local>
2024-07-29 16:32:47 -0500

    2nd

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,15 @@
+#!/usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const hello = (message) => { };
+const commander_1 = require("commander");
+// Delare the program
+const program = new commander_1.Command();
+// Add actions onto that CLI
+program
+    .argument("<sting>", "string to log")
+    .action((message) => {
+    console.log(`Hello ${message}!`);
+})
+    .description("Say hello");
+// Execute the CLI with the given arguments
+program.parse(process.argv);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-const hello = (message: string) => {};
+#!/usr/bin/env node
+
+import { Command } from 'commander'
+
+// Delare the program
+const program = new Command();
+
+// Add actions onto that CLI
+program
+.argument("<sting>", "string to log")
+.action((message: string)=>{
+    console.log(`Hello ${message}!`)
+})
+
+.description("Say hello")
+
+// Execute the CLI with the given arguments
+program.parse(process.argv)
