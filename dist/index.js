0a27b8787e99f6c5a96dd0133db019d033d3c1d8
Yehong Piao <yehongpiao@Yehongs-iMac.local>
2024-07-29 23:19:23 -0500

    3rd

--- dist/index.js
+++ dist/index.js
@@ -7,9 +7,30 @@ const program = new commander_1.Command();
 // Add actions onto that CLI
 program
     .argument("<sting>", "string to log")
-    .action((message) => {
-    console.log(`Hello ${message}!`);
+    .option("-c, --capitalize", "Capitalize the message")
+    .action((message, opts) => {
+    if (opts.capitalize) {
+        console.log(message.toUpperCase());
+    }
+    else {
+        console.log(message);
+    }
 })
     .description("Say hello");
+program
+    .command('add <numbers...>')
+    .description('Add a list of numbers')
+    .action((numbers) => {
+    const nums = numbers.map(Number);
+    const sum = nums.reduce((acc, num) => acc + num, 0);
+    console.log(`The sum is: ${sum}`);
+});
+program
+    .command("get-max-number <numbers...>")
+    .action((numbers) => {
+    const max = Math.max(...numbers);
+    console.log(`Max: ${max}`);
+})
+    .description("Add numbersand log the total");
 // Execute the CLI with the given arguments
 program.parse(process.argv);
